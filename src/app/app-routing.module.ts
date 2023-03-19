05d0ac997e0b870a1af92ff9afc8b890278b2c31
sebasVasco <skaterionegro9@gmail.com>
2023-03-19 12:39:52 -0500

    routing features module to be home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: MainLayoutComponent,
+    loadChildren: () => import('@app/features/features.module').then((m) => m.FeaturesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
