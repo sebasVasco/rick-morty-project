4592cf21da6dc71e4474b5fe4522207f7c80b2ff
sebasVasco <skaterionegro9@gmail.com>
2023-03-19 15:45:32 -0500

    adding routes and links

---
+++ src/app/features/episode/episode.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EpisodeRoutingModule } from './episode-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, EpisodeRoutingModule],
+})
+export class EpisodeModule {}
--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'character',
+  },
+  {
+    path: 'character',
+    loadChildren: () => import('./character/character.module').then((m) => m.CharacterModule),
+  },
+  {
+    path: 'episode',
+    loadChildren: () => import('./episode/episode.module').then((m) => m.EpisodeModule),
+  },
+  {
+    path: 'location',
+    loadChildren: () => import('./location/location.module').then((m) => m.LocationModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
