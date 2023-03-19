d283192a83aaa4fdd3afad9efda2d9961266e1bc
sebasVasco <skaterionegro9@gmail.com>
2023-03-19 12:38:26 -0500

    test: removing specs app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,19 @@
-import { DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
-  let appComponent: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, CoreModule],
       declarations: [AppComponent],
     }).compileComponents();
-    fixture = TestBed.createComponent(AppComponent);
-    appComponent = fixture.componentInstance;
   });
 
   it('should create the appComponent', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const appComponent = fixture.componentInstance;
     expect(appComponent).toBeTruthy();
-  });
-
-  it('should display the app-main-layout component', () => {
-    const appDebugEl: DebugElement = fixture.debugElement;
-    const appMainLayoutComponent = appDebugEl.query(By.css('app-main-layout'));
-    expect(appMainLayoutComponent).toBeTruthy();
   });
 });
