01de125213579769a15692d4962baac984ddcd85
sebasVasco <skaterionegro9@gmail.com>
2023-03-19 12:06:07 -0500

    test: added spec to look if app-main-layout is present

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let appComponent: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, CoreModule],
       declarations: [AppComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    appComponent = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the appComponent', () => {
+    expect(appComponent).toBeTruthy();
+  });
+
+  it('should display the app-main-layout component', () => {
+    const appDebugEl: DebugElement = fixture.debugElement;
+    const appMainLayoutComponent = appDebugEl.query(By.css('app-main-layout'));
+    expect(appMainLayoutComponent).toBeTruthy();
   });
 });
